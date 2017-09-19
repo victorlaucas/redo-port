f38c70104fcdeb0d1f443bda92940522f115b181
Victor <victorlaucas@gmail.com>
2017-09-19 14:15:24 -0600

    Updated about me page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_cable.allowed_request_origins = ['https://vl-portfolio-project.herokuapp.com', 'http://vl-portfolio-project.herokuapp.com']
-  config.action_cable.url = "wss://vl-portfolio-project..herokuapp.com/cable"
+  config.action_cable.url = "wss://vl-portfolio-project.herokuapp.com/cable"
 end
