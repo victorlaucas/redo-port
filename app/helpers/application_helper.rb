125ece63e8186de6a26d7eb440d619adf1472379
Victor <victorlaucas@gmail.com>
2017-10-31 23:11:56 -0600

    Updated blog to have same header as homepage. Currently works, but styling is still in progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,10 +42,10 @@ module ApplicationHelper
         url: portfolios_path,
         title: 'Portfolio'
       },
-      {
-        url: tech_news_path,
-        title: 'Tech News'
-      },
+      # {
+      #   url: tech_news_path,
+      #   title: 'Tech News'
+      # },
     ]
   end
 
